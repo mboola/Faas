acd3eff775d6a5075f763d46f55689dae3e1747c
Marcel Povill <marcel.povill@estudiants.urv.cat>
2023-11-11 21:16:23 +0100

    Testing of reflection started

---
+++ testing_reflection/Controller.java
@@ -0,0 +1,147 @@
+package application;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Future;
+import java.util.function.Function;
+
+import dynamic_proxy.ActionProxy;
+import faas_exceptions.NoActionRegistered;
+import faas_exceptions.NoInvokerAvaiable;
+import faas_exceptions.NoResultAvaiable;
+import policy_manager.PolicyManager;
+
+public class Controller implements ActionProxy{
+	private Map<String, Action<Integer, Object>>	actions;
+	public MetricSet								metrics;
+	private List<Invoker>							invokers;
+	private PolicyManager							policyManager;
+
+	public static Controller instantiate() {
+		if (unicInstance == null)
+			unicInstance = new Controller();
+		return (unicInstance);
+	}
+	protected Controller() {
+		invokers = new LinkedList<Invoker>();
+		actions = new HashMap<String, Action<Integer, Object>>();
+		metrics = new MetricSet();
+	}
+	private static Controller unicInstance = null;
+
+	public void registerInvoker(Invoker invoker)
+	{
+		//TODO: if invoker is already at list throw error
+		invokers.add(invoker);
+	}
+
+	public void deleteInvoker(Invoker invoker)
+	{
+		//TODO: if invoker is not in the list throw error
+		invokers.remove(invoker);
+	}
+
+	/* Used to search if we already have this action in our map */
+	public boolean hasMapAction(String id)
+	{
+		if ( actions.isEmpty() )
+			return (false);
+		if ( actions.get(id) == null )
+			return (false);
+		return (true);
+	}
+
+	//used the register an Action in the controller. Ram must be inputed in MegaBytes
+	public void registerAction(String id, Object f, int ram)
+	{
+		if ( !hasMapAction(id) )
+		{
+			actions.put(id, new Action<Integer, Object>(ram, f));
+			return ;
+		}
+		//TODO: throw error. already exists
+	}
+
+	public void	listActions()
+	{
+		//TODO: is this all the info I wanna show?
+		if ( actions.isEmpty())
+			return ;
+		for(String key : actions.keySet())
+			System.out.println(key);
+	}
+	
+	public void	addPolicyManager(PolicyManager policyManager)
+	{
+		this.policyManager = policyManager;
+	}
+
+	private Invoker selectInvoker(int ram) throws NoInvokerAvaiable
+	{
+		return (policyManager.getInvoker(invokers, ram));
+	}
+
+	private <T, R> R getResult(Action<Integer, Object> action, T args, String id) throws Exception
+	{
+		Invoker	invoker;
+
+		invoker = selectInvoker(action.getRam());
+		return (invoker.invoke(action, args, id));
+	}
+
+	public <T, R> R invoke(String id, T args) throws Exception
+	{
+		Action<Integer, Object>	action;
+
+		if ( !hasMapAction(id) )
+			throw new NoResultAvaiable("Function" + id + "not registered.");
+		action = actions.get(id);
+		return (getResult(action, args, id));
+	}
+
+	public <T, R> List<R> invoke(String id, List<T> args) throws Exception
+	{
+		Action<Integer, Object>	action;
+		List<R> 				result;
+
+		if ( !hasMapAction(id) )
+		{
+			//TODO: throw error, we dont have this action in our map
+			System.out.println("Error");
+			return (null);
+		}
+		result = new LinkedList<R>();
+		action = actions.get(id);
+		for (T element : args)
+			result.add(getResult(action, element, id));
+		return (result);
+	}
+
+	public <T, R> Future<R> invoke_async(String id, T args) throws Exception
+	{
+		Action<Integer, Object>	action;
+
+		action = actions.get(id);
+		if (action == null)
+			throw new NoActionRegistered("There are no actions with that id");
+		return (selectInvoker(action.getRam()).invokeAsync(action, args, id));
+	}
+
+	public void removeAction(String id)
+	{
+		if ( !hasMapAction(id) )
+		{
+			//TODO: throw error, we dont have this action in our map
+			return ;
+		}
+		actions.remove(id);
+	}
+
+	public void shutdownAllInvokers()
+	{
+		for (Invoker invoker:invokers) {
+			invoker.shutdownInvoker();
+		}
+	}
+}
---
+++ testing_reflection/Faas.java
@@ -0,0 +1,109 @@
+package application;
+import java.time.Duration;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Future;
+import java.util.function.Function;
+
+import faas_exceptions.NoInvokerAvaiable;
+import observer.TimerObserver;
+import policy_manager.PolicyManager;
+import policy_manager.RoundRobin;
+
+public class Faas {
+    public static void main(String[] args) throws Exception {
+        Controller		controller;
+		Integer			result;
+		List<Object>	resultList;
+		Invoker			invoker1, invoker2;
+		PolicyManager	policyManager;
+
+		controller = Controller.instantiate();
+		Invoker.addObserver(new TimerObserver());
+		Invoker.setController(controller);
+		
+		invoker1 = Invoker.createInvoker(1);
+		//invoker2 = Invoker.createInvoker(2);
+		controller.registerInvoker(invoker1);
+		//controller.registerInvoker(invoker2);
+		policyManager = new RoundRobin();
+		controller.addPolicyManager(policyManager);
+		//Function<Map<String, Integer>, Integer> f1 = x -> x.get("x") - x.get("y");
+		//controller.registerAction("sub", f1, 2);
+		
+		/*
+		 * try {
+			result = (Integer) controller.invoke("sub", Map.of("x", 1, "y", 2));
+			System.out.println(result);
+		}
+		catch (NoInvokerAvaiable e1) {
+			System.out.println(e1.getMessage());
+		}
+		controller.listActions();
+
+		List<Object> input = Arrays.asList(
+			Map.of("x", 2, "y", 3),
+			Map.of("x", 9, "y", 1),
+			Map.of("x", 8, "y", 8),
+			Map.of("x", 100000000, "y", -10),
+			Map.of("x", 1, "y", 8123),
+			Map.of("x", 2, "y", 4418),
+			Map.of("x", 85, "y", 312348),
+			Map.of("x", 812312312, "y", -4444128)
+		);
+
+		try {
+			resultList = controller.invoke("sub", input);
+			for (Object res : resultList) {
+				System.out.println((Integer)res);
+			}
+		}
+		catch (NoInvokerAvaiable e1) {
+			System.out.println(e1.getMessage());
+		}
+		System.out.println("Start of time.");
+		controller.metrics.showTime("sub");
+		System.out.println("End of time.");
+		 * 
+		 */
+
+		//test async
+		Function<Integer, String> sleep = s -> {
+			try {
+				Thread.sleep(Duration.ofSeconds(s).toMillis());
+				return "Done!";
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+		};
+
+		controller.registerAction("sleepAction", sleep, 1);
+		try {
+			long currentTimeMillis = System.currentTimeMillis();
+			Future<String> fut;
+			List<Future<String>> resList = new LinkedList<Future<String>>();
+			for(int i = 0; i < 2; i++)
+			{
+				fut = controller.invoke_async("sleepAction", 2);
+				resList.add(fut);
+			}
+			List<String> stringsResult = new LinkedList<String>();
+			for (Future<String> future : resList) {
+				stringsResult.add(future.get());
+			}
+			for (String str : stringsResult) {
+				System.out.println(str);
+			}
+			long finalimeMillis = System.currentTimeMillis();
+			System.out.println(" Seconds:" + (finalimeMillis - currentTimeMillis));
+		}
+		catch (NoInvokerAvaiable e1) {
+			System.out.println(e1.getMessage());
+		}
+
+		Invoker.printCache();
+
+		controller.shutdownAllInvokers();
+    }
+}
