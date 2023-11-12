c15e5511e75f9b02ed1a34afa7c7363bf5a15bc8
Marcel Povill <marcel.povill@estudiants.urv.cat>
2023-11-12 12:53:30 +0100

    minor changes at Client

--- testing_reflection/Client.java
+++ testing_reflection/Client.java
@@ -13,13 +13,26 @@ public class Client {
         //this is the application client will call
 		ActionProxy actionProxy = (ActionProxy) DynamicProxy.instantiate(controller);
 
-        //** here actionProxy doesnt have the method suma */
+        //** here actionProxy doesn't have the method suma and only has the interface ActionProxy */
+        //Interfaces of Proxy:
+        /*
+        for (Class<?> interf : actionProxy.getClass().getInterfaces()) {
+            System.out.println(interf.getName());
+        }
+        */
+        //System.out.println("Acciones disponibles: " + actionProxy.actions());
 
         //we register two actions. each time an action is added, actionProxy should be recompiled
         Function<Map<String, Integer>, Integer> suma = x -> x.get("x") + x.get("y");
         actionProxy.registerAction("suma", suma, 1);
 
-        //** here actionProxy has the method suma */
+        //** here actionProxy has the method suma and the interface ActionProxy and suma */
+        //Interfaces of Proxy:
+        /*
+        for (Class<?> interf : actionProxy.getClass().getInterfaces()) {
+            System.out.println(interf.getName());
+        }
+        */
         //System.out.println("Acciones disponibles: " + actionProxy.actions());
 
         //testing of behaviour (?)
