8c9bc14212067f329fff4a3354998e008aa04f1c
mboola <mpovilleti@gmail.com>
2023-12-09 20:42:13 +0100

    Added more tests to RoundRobin.

---
+++ src/testing/InvocationTester.java
@@ -0,0 +1,72 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.Duration;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.Future;
+import java.util.function.Function;
+
+import application.Controller;
+import invoker.Invoker;
+
+public class InvocationTester {
+
+	protected void initializeSleepAction(String name, int ram, Controller controller)
+	{
+		Function<Integer, String> sleep = s -> {
+			long time = Duration.ofMillis(s).toMillis();
+			try {
+				Thread.sleep(time);
+				return "Done!";
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+		};
+		try {
+			controller.registerAction(name, sleep, ram);
+		}
+		catch (Exception e) {
+			assertTrue(false);
+		}
+	}
+
+	protected void createAndAddInvokers(List<Long> ramInvokers, Controller controller)
+	{
+		Invoker invoker;
+		for (Long ram : ramInvokers) {
+			invoker = Invoker.createInvoker(ram);
+			try {
+				controller.registerInvoker(invoker);
+			}
+			catch (Exception e) {
+			}
+		}
+	}
+
+	protected <T, R> List<R> invokeList(String id, int numCalls, T data, boolean delay, Controller controller)
+	{
+		List<Future<R>> futures = new LinkedList<Future<R>>();
+		List<R> result = new LinkedList<R>();
+		
+		for (int i = 0; i < numCalls; i++) {
+			try {
+				futures.add(controller.invoke_async(id, data));
+				if (delay)
+					Thread.sleep(1000);
+			}
+			catch (Exception e) {
+			}
+		}
+		for (Future<R> future : futures) {
+			try {
+				result.add(future.get());
+			}
+			catch (Exception e) {
+			}
+		}
+		return (result);
+	}
+
+}
--- src/testing/policy_manager/TestRoundRobin.java
+++ src/testing/policy_manager/TestRoundRobin.java
@@ -4,12 +4,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.time.Duration;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Future;
 import java.util.function.Function;
 
 import org.junit.Before;
@@ -21,11 +18,11 @@ import application.Controller;
 import faas_exceptions.NoInvokerAvailable;
 import faas_exceptions.OperationNotValid;
 import invoker.Invoker;
-import observer.IdObserver;
 import observer.InvocationObserver;
 import policy_manager.RoundRobin;
+import testing.InvocationTester;
 
-public class TestRoundRobin {
+public class TestRoundRobin extends InvocationTester {
 
 	private Controller controller;
 
@@ -35,37 +32,15 @@ public class TestRoundRobin {
 		controller = Controller.instantiate();
 		Invoker.setController(controller);
 
-		Function<Integer, String> sleep = s -> {
-			long time = Duration.ofMillis(s).toMillis();
-			try {
-				Thread.sleep(time);
-				return "Done!";
-			} catch (InterruptedException e) {
-				throw new RuntimeException(e);
-			}
-		};
 		Function<Map<String, Integer>, Integer> f = x -> x.get("x") + x.get("y");
 		Invoker.addObserver(new InvocationObserver());
 
 		try {
 			controller.setPolicyManager(new RoundRobin());
-			controller.registerAction("Sleep", sleep, 1);
+			initializeSleepAction("Sleep", 1, controller);
 			controller.registerAction("Add", f, 1);
 		} catch (OperationNotValid e) {
 			assertTrue(false);
-		}
-	}
-
-	private void createAndAddInvokers(List<Long> ramInvokers)
-	{
-		Invoker invoker;
-		for (Long ram : ramInvokers) {
-			invoker = Invoker.createInvoker(ram);
-			try {
-				controller.registerInvoker(invoker);
-			}
-			catch (Exception e) {
-			}
 		}
 	}
 
@@ -73,7 +48,7 @@ public class TestRoundRobin {
 	public void testRoundRobinExceptions()
 	{
 		assertThrows(NoInvokerAvailable.class, () -> controller.invoke("Add", 1));
-		createAndAddInvokers(Arrays.asList(1L));
+		createAndAddInvokers(Arrays.asList(1L), controller);
 		Action factorial = new FactorialAction();
 		try {
 			controller.registerAction("Factorial", factorial, 2);
@@ -85,9 +60,9 @@ public class TestRoundRobin {
 	}
 
 	@Test
-	public void testRoundRobinOneInvokerSyncFunc()
+	public void testRoundRobinOneInvokerSyncFuncA()
 	{
-		createAndAddInvokers(Arrays.asList(1L));
+		createAndAddInvokers(Arrays.asList(1L), controller);
 		try {
 			List<Integer> result = controller.invoke("Add", 
 				Arrays.asList(Map.of("x", 2, "y", 1), Map.of("x", 4, "y", 2), Map.of("x", 7, "y", 3)));
@@ -100,33 +75,70 @@ public class TestRoundRobin {
 	}
 
 	@Test
-	public void testRoundRobinOneInvokerAsyncFunc()
+	public void testRoundRobinOneInvokerSyncFuncB()
 	{
-		createAndAddInvokers(Arrays.asList(1L));
+		createAndAddInvokers(Arrays.asList(2L), controller);
 		try {
-			long currentTimeMillis = System.currentTimeMillis();
-			List<Future<String>> futures = new LinkedList<Future<String>>();
-			futures = controller.invoke_async("Sleep", Arrays.asList(2000, 2000, 2000));
-			List<String> stringsResult = new LinkedList<String>();
-
-			for (Future<String> future : futures)
-				stringsResult.add(future.get());
-			
-			long totalTime = System.currentTimeMillis() - currentTimeMillis;
-
-			assertTrue(totalTime < 6200 && totalTime > 5800);
+			List<Integer> result = controller.invoke("Add", 
+				Arrays.asList(Map.of("x", 2, "y", 1), Map.of("x", 4, "y", 2), Map.of("x", 7, "y", 3)));
 		}
 		catch (Exception e){
 			assertTrue(false);
 		}
+		String str = controller.metrics.getData("InvocationObserver", "Add");
+		assertEquals("000", str);
+	}
+
+	@Test
+	public void testRoundRobinOneInvokerAsyncFuncA()
+	{
+		List<String> stringsResult;
+
+		createAndAddInvokers(Arrays.asList(1L), controller);
+
+		try {
+			long currentTimeMillis = System.currentTimeMillis();
+			stringsResult = invokeList("Sleep", 3, 3000, true, controller);
+			long totalTime = System.currentTimeMillis() - currentTimeMillis;
+
+			if (totalTime > 9200 || totalTime < 8800) assertTrue(false);
+			else assertTrue(true);
+		}
+		catch (Exception e) {
+			assertTrue(false);
+		}
+
 		String str = controller.metrics.getData("InvocationObserver", "Sleep");
 		assertEquals("000", str);
 	}
 
 	@Test
-	public void testRoundRobinTwoInvokerSameRamSyncFunc()
+	public void testRoundRobinOneInvokerAsyncFuncB()
 	{
-		createAndAddInvokers(Arrays.asList(1L, 1L));
+		List<String> stringsResult;
+
+		createAndAddInvokers(Arrays.asList(2L), controller);
+
+		try {
+			long currentTimeMillis = System.currentTimeMillis();
+			stringsResult = invokeList("Sleep", 3, 3000, true, controller);
+			long totalTime = System.currentTimeMillis() - currentTimeMillis;
+
+			if (totalTime > 6200 || totalTime < 5800) assertTrue(false);
+			else assertTrue(true);
+		}
+		catch (Exception e) {
+			assertTrue(false);
+		}
+
+		String str = controller.metrics.getData("InvocationObserver", "Sleep");
+		assertEquals("000", str);
+	}
+
+	@Test
+	public void testRoundRobinTwoInvokerSameRamSyncFuncA()
+	{
+		createAndAddInvokers(Arrays.asList(1L, 1L), controller);
 		try {
 			List<Integer> result = controller.invoke("Add", 
 				Arrays.asList(Map.of("x", 2, "y", 1), Map.of("x", 4, "y", 2), Map.of("x", 7, "y", 3), Map.of("x", 7, "y", 2)));
@@ -139,25 +151,52 @@ public class TestRoundRobin {
 	}
 
 	@Test
-	public void testRoundRobinTwoInvokerSameRamAsyncFunc()
+	public void testRoundRobinTwoInvokerSameRamSyncFuncB()
 	{
-		createAndAddInvokers(Arrays.asList(1L, 1L));
+		createAndAddInvokers(Arrays.asList(2L, 2L), controller);
 		try {
-			long currentTimeMillis = System.currentTimeMillis();
-			List<Future<String>> futures = new LinkedList<Future<String>>();
-			futures = controller.invoke_async("Sleep", Arrays.asList(1500, 1600, 2500, 2400, 3000, 3000));
-			List<String> stringsResult = new LinkedList<String>();
-
-			for (Future<String> future : futures)
-				stringsResult.add(future.get());
-			
-			long totalTime = System.currentTimeMillis() - currentTimeMillis;
-
-			assertTrue(totalTime < 7200 && totalTime > 6800);
+			List<Integer> result = controller.invoke("Add", 
+				Arrays.asList(Map.of("x", 2, "y", 1), Map.of("x", 4, "y", 2), Map.of("x", 7, "y", 3), Map.of("x", 7, "y", 2)));
 		}
 		catch (Exception e){
 			assertTrue(false);
 		}
+		String str = controller.metrics.getData("InvocationObserver", "Add");
+		assertEquals("1010", str);
+	}
+
+	@Test
+	public void testRoundRobinTwoInvokerSameRamAsyncFuncA()
+	{
+		List<String> stringsResult;
+
+		createAndAddInvokers(Arrays.asList(1L, 1L), controller);
+
+		try {
+			stringsResult = invokeList("Sleep", 6, 10000, true, controller);
+		}
+		catch (Exception e) {
+			assertTrue(false);
+		}
+
+		String str = controller.metrics.getData("InvocationObserver", "Sleep");
+		assertEquals("101010", str);
+	}
+
+	@Test
+	public void testRoundRobinTwoInvokerSameRamAsyncFuncB()
+	{
+		List<String> stringsResult;
+
+		createAndAddInvokers(Arrays.asList(2L, 2L), controller);
+
+		try {
+			stringsResult = invokeList("Sleep", 6, 10000, true, controller);
+		}
+		catch (Exception e) {
+			assertTrue(false);
+		}
+
 		String str = controller.metrics.getData("InvocationObserver", "Sleep");
 		assertEquals("101010", str);
 	}
@@ -165,7 +204,7 @@ public class TestRoundRobin {
 	@Test
 	public void testRoundRobinTwoInvokerDifferentRamSyncFunc()
 	{
-		createAndAddInvokers(Arrays.asList(1L, 2L));
+		createAndAddInvokers(Arrays.asList(1L, 2L), controller);
 		try {
 			List<Integer> result = controller.invoke("Add", 
 				Arrays.asList(Map.of("x", 2, "y", 1), Map.of("x", 4, "y", 2), Map.of("x", 7, "y", 3), Map.of("x", 7, "y", 2)));
@@ -177,39 +216,15 @@ public class TestRoundRobin {
 		assertEquals("1010", str);
 	}
 
-	private <T, R> List<R> invokeList(String id, int numCalls, T data, boolean delay)
-	{
-		List<Future<R>> futures = new LinkedList<Future<R>>();
-		List<R> result = new LinkedList<R>();
-		
-		for (int i = 0; i < numCalls; i++) {
-			try {
-				futures.add(controller.invoke_async(id, data));
-				if (delay)
-					Thread.sleep(1000);
-			}
-			catch (Exception e) {
-			}
-		}
-		for (Future<R> future : futures) {
-			try {
-				result.add(future.get());
-			}
-			catch (Exception e) {
-			}
-		}
-		return (result);
-	}
-
 	@Test
 	public void testRoundRobinTwoInvokerDifferentRamAsyncFuncA()
 	{
 		List<String> stringsResult;
 
-		createAndAddInvokers(Arrays.asList(2L, 1L));
+		createAndAddInvokers(Arrays.asList(2L, 1L), controller);
 
 		try {
-			stringsResult = invokeList("Sleep", 6, 10000, true);
+			stringsResult = invokeList("Sleep", 6, 10000, true, controller);
 		}
 		catch (Exception e) {
 		}
@@ -223,10 +238,10 @@ public class TestRoundRobin {
 	{
 		List<String> stringsResult;
 
-		createAndAddInvokers(Arrays.asList(1L, 2L));
+		createAndAddInvokers(Arrays.asList(1L, 2L), controller);
 
 		try {
-			stringsResult = invokeList("Sleep", 6, 10000, true);
+			stringsResult = invokeList("Sleep", 6, 10000, true, controller);
 		}
 		catch (Exception e) {
 		}
@@ -234,4 +249,22 @@ public class TestRoundRobin {
 		String str = controller.metrics.getData("InvocationObserver", "Sleep");
 		assertEquals("101010", str);
 	}
+
+	@Test
+	public void testRoundRobinThreeInvokerSameRamSyncFunc()
+	{
+		createAndAddInvokers(Arrays.asList(1L, 1L, 1L), controller);
+		try {
+			List<Integer> result = controller.invoke("Add", 
+				Arrays.asList(Map.of("x", 2, "y", 1), Map.of("x", 4, "y", 2), Map.of("x", 7, "y", 3), Map.of("x", 7, "y", 2)));
+		}
+		catch (Exception e){
+			assertTrue(false);
+		}
+		String str = controller.metrics.getData("InvocationObserver", "Add");
+		assertEquals("1201", str);
+	}
+
+	
+	
 }
