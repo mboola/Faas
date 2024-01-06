b8592f8690a8aef92f35a2352bb327ff8a31b31f
mboola <mpovilleti@gmail.com>
2024-01-06 13:18:40 +0100

    Distribution of charge commented. Needs implementation.

--- src/policymanager/UniformGroup.java
+++ src/policymanager/UniformGroup.java
@@ -19,7 +19,7 @@ public class UniformGroup implements PolicyManager{
     private boolean executingGroup;
 
     private long groupSize;
-    private long chargeDistributed;
+    private long invocationsDistributed;
 
     private int	lastInvokerAssigned;
 
@@ -27,17 +27,18 @@ public class UniformGroup implements PolicyManager{
     private void setSingleUniformValues()
     {
         groupSize = 1;
-        chargeDistributed = 0;
+        invocationsDistributed = 0;
     }
 
-    private void setGroupUniformValues(List<InvokerInterface> invokers, int numInvocations, long ram)
+    private void setGroupUniformValues(List<InvokerInterface> invokers, int numInvocations, long ram) throws NoInvokerAvailable
     {
         //get the average of invokers ram available, num of invokers, and how may invokers can execute the function
-        long    numInvokersMaxRamAvailable;
-        long    numInvokersRamAvailable;
+        long    invokersMaxRamUsable;
+        long    invokersRamAvailable;
+        double  averageRamAvailable;
         
         //here I get the num of invokers that can execute the function
-        numInvokersMaxRamAvailable = invokers.stream()
+        invokersMaxRamUsable = invokers.stream()
             .filter(value -> {
                 try {
                     return value.getMaxRam() >= ram;
@@ -46,9 +47,12 @@ public class UniformGroup implements PolicyManager{
                 }
             })
             .count();
+        //If not a single invoker can execute this function
+        if (invokersMaxRamUsable == 0)
+            throw new NoInvokerAvailable("No Invoker Avaiable with at least " + ram + " RAM.");
 
-        //and here I get the num of invokers that have enough ram to execute
-        numInvokersRamAvailable = invokers.stream()
+        //and here I get the invokers that have available ram
+        invokersRamAvailable = invokers.stream()
             .filter(value -> {
                 try {
                     return value.getAvailableRam() >= ram;
@@ -57,10 +61,38 @@ public class UniformGroup implements PolicyManager{
                 }
             })
             .count();
+
+        //and here I get the average of those who have available ram
+        averageRamAvailable = invokers.stream()
+            .filter(value -> {
+                try {
+                    return value.getAvailableRam() >= ram;
+                } catch (RemoteException e) {
+                    return false;
+                }
+            })
+            .mapToDouble(value -> {
+                try {
+                    return value.getAvailableRam();
+                } catch (RemoteException e) {
+                    return 0;
+                }
+            })
+            .average()
+            .orElse(0.0);
         
-        if (numInvokersRamAvailable )
-        
-        
+        //if the charge can be distributed between the available invokers and their ram
+        if (averageRamAvailable * invokersRamAvailable >= numInvocations * ram)
+        {
+            //we will distribute the charge to only available ones.
+            
+        }
+        else
+        {
+            //if it cannot be distributed to only available ones, we distribute it
+            //to all of them (that can execute it)
+
+        }
     }
 
     public UniformGroup() {
