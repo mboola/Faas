cb8f5e294cfb82fa408c8618910b8b3347e81dcc
mboola <mpovilleti@gmail.com>
2024-01-09 18:06:14 +0100

    Scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -32,25 +32,6 @@ object Main {
 		}
 	}
 
-	//TimerDecorator
-	//def timerDecorator[T, R](function: T => R): T => R = { 
-	//	(arg: T) => {
-	//		val startTime = System.nanoTime()
-	//		val result = function(arg)
-	//		val endTime = System.nanoTime()
-	//		println(s"Time of execution is ${endTime - startTime} ns.")
-	//		result
-	//	}
-	//}
-
-	// Cache function, utilizando el mapa mutable de Scala.
-	// Locura el getOrElseUpdate. Guarda el argumento (sea el que sea, pudiendo ser una funcion...)
-	// Junto con el resultado de su ejecución. 
-	//def cacheDecorator[T, R](function: T => R): T => R = {
-	//	val cache = collection.mutable.Map[T, R]()
-	//	(arg: T) => cache.getOrElseUpdate(arg, function(arg))
-	//}
-
 	def wordCount(text: String): Map[String, Int] = {
 		val words = text.toLowerCase.split("\\W+").filter(_.nonEmpty)	//aqui el \\W+ místico lo que hace es separar por carácteres que NO sean palabras.
 		words.groupBy(identity).view.mapValues(_.length).toMap			//el filter se le pasa una funcion sin todos los parámetros, que básicamente serán todas las palabras y nos guaradará
