78076fd59f80bdea71ca31c9ebc5ddaae0e14f13
mboola <mpovilleti@gmail.com>
2024-01-14 22:22:01 +0100

    Final push

--- src/services/countwords/UltimateCountWordsMain.java
+++ src/services/countwords/UltimateCountWordsMain.java
@@ -1,18 +1,18 @@
 package services.countwords;
 
-import java.util.LinkedList;
-import java.util.List;
 import java.util.concurrent.Future;
 import java.util.function.Function;
 
 import core.application.Action;
 import core.application.Controller;
-import core.dynamicproxy.DynamicProxy;
 import core.invoker.Invoker;
+import core.metrics.MetricCollection;
 import decorator.TimerDecorator;
+import observer.TimerObserver;
 import policymanager.BigGroup;
 import policymanager.GreedyGroup;
 import policymanager.RoundRobin;
+import policymanager.UniformGroup;
 
 @SuppressWarnings({"unchecked"})
 public class UltimateCountWordsMain {
@@ -29,12 +29,13 @@ public class UltimateCountWordsMain {
 				return (new CountWords());
 			}
 		;
-
+		TimerObserver timerObserver = new TimerObserver();
 		try {
+			MetricCollection.instantiate().addObserver(timerObserver);
 			controller.registerInvoker(invoker);
 			controller.registerInvoker(invoker1);
 			controller.registerInvoker(invoker2);
-			controller.setPolicyManager(new RoundRobin());
+			controller.setPolicyManager(new BigGroup());
 			controller.registerAction("CountWordsService", countWords, 1);
 
 			Action ultimateCountWords = new CountWordsAction();
@@ -48,7 +49,20 @@ public class UltimateCountWordsMain {
 		try {
 			Future<Long> result = controller.invoke_async("ultimateCountWords", "src\\services\\countwords\\test.txt");
 			System.out.println(result.get());
+			result = controller.invoke_async("ultimateCountWords", "src\\services\\countwords\\test.txt");
+			System.out.println(result.get());
+			result = controller.invoke_async("ultimateCountWords", "src\\services\\countwords\\test.txt");
+			System.out.println(result.get());
+			result = controller.invoke_async("ultimateCountWords", "src\\services\\countwords\\test.txt");
+			System.out.println(result.get());
+			result = controller.invoke_async("ultimateCountWords", "src\\services\\countwords\\test.txt");
+			System.out.println(result.get());
 			controller.shutdownAllInvokers();
+			System.out.println("Total   time: " + timerObserver.calculateAllTime("ultimateCountWords") + "ns.");
+			System.out.println("Minimum time: " + timerObserver.calculateMinTime("ultimateCountWords") + "ns.");
+			System.out.println("Maximum time: " + timerObserver.calculateMaxTime("ultimateCountWords") + "ns.");
+			System.out.println("Average time: " + timerObserver.calculateAverageTime("ultimateCountWords") + "ns.");
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
